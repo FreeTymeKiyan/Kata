027f61f7abeafb6934b18910c2cdcf868d084315
Kiyan <freetymesunkiyan@gmail.com>
2014-10-10 13:03:47 -0400

    Modified project structure.

---
+++ Searching/BinarySearch/BinarySearch2.java
@@ -0,0 +1,24 @@
+class BinarySearch2 {
+
+    public static int binarySearch(int[] a, int x) {
+        int left = 0;
+        int right = a.length - 1;
+        int mid;
+
+        while(left <= right) {
+            mid = (left + right) / 2;
+            if(a[mid] == x) return mid;
+            else if(a[mid] > x) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+
+    public static int binarySearchRecursive(int[] a, int x, int left, int right) {
+        if(left > right) return -1;
+        int mid = (left + right) / 2;
+        if(a[mid] == x) return mid;
+        else if(a[mid] > x) return binarySearchRecursive(a, x, left, mid - 1);
+        else return binarySearchRecursive(a, x, mid + 1, right);
+    }
+}
---
+++ Searching/BinarySearch/Main.java
@@ -0,0 +1,41 @@
+class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        //System.out.println(BinarySearch.binarySearch(array, 2));
+        //System.out.println(BinarySearch.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch1.binarySearch(array, 2));
+        //System.out.println(BinarySearch1.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch2.binarySearch(array, 2));
+        //System.out.println(BinarySearch2.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch3.binarySearch(array, 2));
+        //System.out.println(BinarySearch3.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch4.binarySearch(array, 2));
+        //System.out.println(BinarySearch4.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch5.binarySearch(array, 2));
+        //System.out.println(BinarySearch5.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch6.binarySearch(array, 2));
+        //System.out.println(BinarySearch6.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch7.binarySearch(array, 2));
+        //System.out.println(BinarySearch7.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch8.binarySearch(array, 2));
+        //System.out.println(BinarySearch8.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch9.binarySearch(array, 2));
+        //System.out.println(BinarySearch9.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch10.binarySearch(array, 2));
+        //System.out.println(BinarySearch10.binarySearchRecursive(array, 9, 0, array.length - 1));
+        //System.out.println(BinarySearch11.binarySearch(array, 2));
+        //System.out.println(BinarySearch11.binarySearchRecursive(array, 19, 0, array.length - 1));
+        //System.out.println(BinarySearch12.binarySearch(array, 2));
+        //System.out.println(BinarySearch12.binarySearchRecursive(array, 19, 0, array.length - 1));
+        // System.out.println(BinarySearch13.binarySearch(array, 2));
+        // System.out.println(BinarySearch13.binarySearchRecursive(array, 19, 0, array.length - 1));
+        // System.out.println(BinarySearch14.binarySearch(array, 2));
+        // System.out.println(BinarySearch14.binarySearchRecursive(array, 19, 0, array.length - 1));        
+        // System.out.println(BinarySearch15.binarySearch(array, 2));
+        // System.out.println(BinarySearch15.binarySearchRecursive(array, 19, 0, array.length - 1));   
+        System.out.println(BinarySearch16.binarySearch(array, 2));
+        System.out.println(BinarySearch16.binarySearchRecursive(array, 19, 0, array.length - 1));
+
+    }
+}
