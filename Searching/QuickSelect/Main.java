85f8d3160319d6ebc84d523d744950ed141ad59f
Kiyan <freetymesunkiyan@gmail.com>
2015-01-11 12:56:41 -0500

    Added quick select

--- Searching/QuickSelect/Main.java
+++ Searching/QuickSelect/Main.java
@@ -1,9 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    int[] a         = {21, 3, 34, 5, 13, 8, 2, 55, 1, 19};
-    int expResult[] = {1, 2, 3, 5, 8, 13, 19, 21, 34, 55};
     int k           = 1;
+    int expResult[] = {1, 2, 3, 5, 8, 13, 19, 21, 34, 55};
+
     for (int exp:expResult) {
+      int[] a = {21, 3, 34, 5, 13, 8, 2, 55, 1, 19};
+      System.out.println(k);
       System.out.println(exp);
       System.out.println(QuickSelect.select(a, k++));
       System.out.println("---");
--- Searching/QuickSelect/QuickSelect.java
+++ Searching/QuickSelect/QuickSelect.java
@@ -1,5 +1,7 @@
 class QuickSelect {
   public static int select(int[] a, int k) {
+      print(a);
+      
     if (k > a.length || k <= 0) {
       return -1; // invalid index
     }
@@ -8,22 +10,48 @@ class QuickSelect {
     int right = a.length - 1;
     int index;
     
-    while (left <= right) { // saleft can be equal to right
+    while (left < right) { // saleft can be equal to right
+      System.out.println("left: " + left);
+      System.out.println("right: " + right);
       index = partition(a, left, right);
-      if (index == k) {
-        return a[k - 1];
-      } else if (index > k) {
+      System.out.println("index: " + index);
+      if (index > k - 1) {
         right = index - 1;
+      } else if (index < k - 1) {
+        left = index + 1;
       } else {
-        left = index;
-      }
+        return a[index];
+      } 
     }
-    
-    return -1;
+      return a[left];
   }
   
+  static void print(int[] num) {
+      for (int a : num) {
+          System.out.print(a + ", ");
+      }
+      System.out.println();
+  }
+  
+  public static void main(String[] args) {
+      int[] a = {21, 3, 34, 5, 13, 8, 2, 55, 1, 19};
+      System.out.println(partition(a, 0, 9));
+      print(a);
+      System.out.println(partition(a, 0, 6));
+      print(a);
+      System.out.println(partition(a, 0, 5));
+      print(a);
+      
+  }
+  
+  /**
+   * wront partition
+   */
   private static int partition(int[] a, int left, int right) {
-    int pivot = a[left + (right - left) / 2];
+    // int pivot = a[left + (right - left) / 2];
+    int pivot = a[left];
+    // System.out.println("pivotIndex: " + (left + (right - left) / 2));
+    // System.out.println("pivot: " + pivot);
     while(left <= right) {
       while(a[left] < pivot) left++;
       while(a[right] > pivot) right--;
@@ -37,4 +65,25 @@ class QuickSelect {
     }
     return left;
   }
+  
+    // private static int partition(int[] a, int left, int right) {
+    //     int pivot = a[left];
+    //     int start = left;
+    //
+    //     while (left <= right) {
+    //         while (a[left] < pivot) left++;
+    //         while (a[right] > pivot) right--;
+    //         if (left <= right) {
+    //             int temp = a[left];
+    //             a[left] = a[right];
+    //             a[right] = temp;
+    //             left++;
+    //             right--;
+    //         }
+    //     }
+    //     int temp2 = a[left];
+    //     a[left] = pivot;
+    //     a[start] = temp2;
+    //     return left;
+    // }
 }
